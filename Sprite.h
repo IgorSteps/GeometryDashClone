c443688f8ad840dc01058f1f1039eb0de5b0e476
Igor Stepanenko <igorstepanenko696@gmail.com>
2022-08-17 20:34:47 +0100

    Spritesheet initlisation and rendering

--- Sprite.h
+++ Sprite.h
@@ -12,10 +12,11 @@
 class Sprite : public Component
 {
 public:
-	Sprite(std::string filename);	//default constructor
-	Sprite(int imgX, int imgY, int tileWidth, int tileHeight);
+	Sprite(std::string filename);	//Sprite constructor 
+	Sprite(std::string filename, float imgX, float imgY, float tileWidth, float tileHeight, float pW, float pH); //Spritesheet constructor
 	~Sprite();
-	void Init(Shader& shader, float colour[3], float repeatInS, float repeatInT);
+
+	void initSprite(Shader& shader, float colour[3], float repeatInS, float repeatInT);
 	void initSubSprite(Shader& shader, float colour[3]);
 	void draw(Shader& shader, glm::mat4& ModelViewMatrix, glm::mat4& ProjectionMatrix);
 
@@ -24,18 +25,14 @@ public:
 private:
 	unsigned int m_vaoID;		// id for Vertex Array Object
 	unsigned int m_vboID[3];	// ids for Vertex Buffer Objects
-	unsigned int m_NumberOfVerts;//number of vertices in the object
-	unsigned int EBO[3];
+	unsigned int m_eboID[3];	// ids for Element Buffer Objects
 	//Dimensions of the sprite.
 	float m_Width;
 	float m_Height;
-
 	//sub sprite data
-	int imgX, imgY, tileWidth, tileHeight;
-
+	float imgX, imgY, tileWidth, tileHeight;
 	// picture file name
-	std::string filename;
-
+	std::string spriteFile, spritesheetFile;
 	//identifier for the texture
 	GLuint m_TexName; 
 	ImageLoading image;
