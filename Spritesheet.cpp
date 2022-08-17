2f7a346424f4056ec4024f83b7c4806e735b8895
Igor Stepanenko <igorstepanenko696@gmail.com>
2022-08-17 20:36:54 +0100

    Spritesheets, some cleanup

---
+++ Spritesheet.cpp
@@ -0,0 +1,22 @@
+#include "Spritesheet.h"
+
+Spritesheet::Spritesheet(std::string file, float tW, float tH, float sp, int columns, int size, float fileW, float fileH) {
+	this->tileHeight = tH;
+	this->tileWidth = tW;
+	this->spacing = sp;
+
+	int row=0, count=0;
+	while (count < size) {
+		for (int column = 0; column < columns; column++) {
+			float imgX = (column * tW) + (column * sp); // top left texture x-coord
+			float imgY = (row * tH) + (row * sp);		// top left texture y-coord
+
+			sprites.push_back(new Sprite(file, imgX, imgY, tW, tH, fileW, fileH));
+			count++;
+			if (count > size - 1) {
+				break;
+			}
+		}
+		row++; 
+	}
+}
