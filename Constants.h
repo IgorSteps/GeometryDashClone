08df85c1c1e4b2942735c834166f90aa44e5f781
Igor Stepanenko <igorstepanenko696@gmail.com>
2022-08-21 15:44:56 +0100

    Created constatns class

---
+++ Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Constants {
+	constexpr int SCREEN_WIDTH = 1280;
+	constexpr int SCREEN_HEIGHT = 720;
+
+	constexpr int PLAYER_WIDTH = 42;
+	constexpr int PLAYER_HEIGHT = 42;
+
+	constexpr int GROUND_Y = 800;
+};
