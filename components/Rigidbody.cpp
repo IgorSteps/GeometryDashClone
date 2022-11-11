3be98f9e19ecc8fa090e7a47b3b7a96f556d01f0
Igor Stepanenko <igorstepanenko696@gmail.com>
2022-11-11 17:48:45 +0000

    fix flying spaceship

--- components/Rigidbody.cpp
+++ components/Rigidbody.cpp
@@ -1,5 +1,6 @@
 #include "Rigidbody.h"
-
+#include "GameObject.h"
+#include "Player.h"
 Rigidbody::Rigidbody(glm::vec2 v) {
 	this->Velocity = v;
 }
@@ -8,8 +9,14 @@ void Rigidbody::update(float dt) {
 	gameObj->transform->position.y += Velocity.y * dt;
 	gameObj->transform->position.x += Velocity.x * dt;
 
-	Velocity.y += Constants::GRAVITY * dt;
-
+	if (gameObj->getComponent<Player>()->m_State == FLY)
+	{
+		Velocity.y += 800 * dt;
+	}
+	else
+	{
+		Velocity.y += Constants::GRAVITY * dt;
+	}
 	if (abs(Velocity.y) > Constants::TERMINAL_VELOCITY) 
 	{
    		Velocity.y = signum(Velocity.y) * Constants::TERMINAL_VELOCITY;
